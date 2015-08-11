5460dba5571bd5f5755fcd1346aff6d285c199ab
Filip Sobalski <filip.sobalski@creativestyle.pl>
2015-08-11 19:08:44 +0200

    APIDebugBundle integration attempt

--- src/ElasticBundle/Client/ClientFactory.php
+++ src/ElasticBundle/Client/ClientFactory.php
@@ -4,10 +4,22 @@ namespace Nimble\ElasticBundle\Client;
 
 use Elasticsearch\Client;
 use Elasticsearch\ClientBuilder;
+use Pinkeen\ApiDebugBundle\Bridge\RingPHP\DataCollectorMiddleware;
 use Psr\Log\LoggerInterface;
 
 class ClientFactory
 {
+    /**
+     * @var DataCollectorMiddleware
+     */
+    private $middleware;
+
+    public function __construct(DataCollectorMiddleware $middleware)
+    {
+        $this->middleware = $middleware;
+    }
+
+
     /**
      * @param array $hosts
      * @param LoggerInterface $logger
@@ -24,6 +36,10 @@ class ClientFactory
         if (null !== $logger) {
             $builder->setLogger($logger);
         }
+
+        $handler = $this->middleware->createHandler(ClientBuilder::defaultHandler(), 'elasticsearch');
+
+        $builder->setHandler($handler);
 
         return $builder->build();
     }
